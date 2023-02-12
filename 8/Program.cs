910fc5a90d4523188f83c8f973e2e2876f137e83
fake_state <ntdako@gmail.com>
2023-02-12 19:01:58 +0400

    add 8

---
+++ 8/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Найдем все четные числа от 1 до любого числа");
+Console.WriteLine("Введите любое число, больше 1");
+int N = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+
+while (index <= N)
+{
+    if (index % 2 == 0) Console.Write($"{index} ");
+index++;
+}
